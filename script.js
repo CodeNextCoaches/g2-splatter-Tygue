d56dabbc85735e6d548f1e1ffda9e4ea62212735
Student <student@Admins-MacBook-Air.local>
2018-02-17 14:19:56 -0500

    I learned about the basics of Java Script

--- script.js
+++ script.js
@@ -1,21 +1,59 @@
+var canvas;
+var canvasWidth = 500;
+var canvasHeight = 400;
+var button;
+var slider;
+var spread;
+
+
 // declare and initialize global variables
 
 /*
  * setup()
  */
+function setup(){
+  button=select("#clear-button");
+  slider=select("#slider");
+  canvas=createCanvas(canvasWidth,canvasHeight);
+  background(0,0,0)
+  canvas.parent("canvas-area");
+  canvas.mousePressed(drawEllipse);
+  button.mousePressed(setup);
+  slider.input(updateSpread);
+  updateSpread();
 
+}
+
+function draw(){
+
+}
 
 /*
  * drawEllipse()
  */
+function drawSplatter(){
+  fill("#ffeeff");
+  ellipse(mouseX, mouseY,spread,spread);
+  noStroke();
+  random(10,15);
+  fill(random(0,255),random(0,255),random(0,255));
+  for(i=0;i<kkdkdk,i++){
+    var radius = random(0,15);
 
+    ellipse(random(mouseX-spread,mouseX+spread),random(mouseY-spread,mouseY+spread),radius)
+  }
+  }
+
+}
 
 /*
  * updateSpread()
  * This function simply sets the spread variable to the value of the slider.
  * For example, if the slider is at 75, the spread will be set to 75 as well.
  */
-
+function updateSpread(){
+  spread=slider.value();
+}
 
  /*
   * drawSplatter()
